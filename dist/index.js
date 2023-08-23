dcf655a0ea5488e01a8d801416b1ea32d9460f56
kskrao219 <keshava.balaji@anywhere.co>
2023-08-23 19:14:36 +0530

    build

--- dist/index.js
+++ dist/index.js
@@ -13417,14 +13417,14 @@ async function addReviewersToPr(){
 
     if(context.payload.action!="labeled") throw `Only labeled action supported , ${context.payload.action || github.context.eventName} is not supported`;
 
-    const prAuthor = context.actor;
-    const debugMode = (core.getInput('debugMode')==true);
+    const prAuthor = context.payload.pull_request.user.login;
+    const debugMode = core.getInput('debugMode')
     const owner = context.payload.repository.owner.login ;
     const repo = context.payload.pull_request.base.repo.name;
     const pull_number = context.payload.pull_request.number;
     const finalReviewers = reviewers.filter(reviewer=> reviewer!=prAuthor);
     console.log(finalReviewers);
-
+    console.log(`context::${context}`);
     if(debugMode){
         core.info(`Final reviewers :::${finalReviewers}`);
         core.info(`context::${JSON.stringify(context)}`)
@@ -13435,7 +13435,7 @@ async function addReviewersToPr(){
         pull_number,
         reviewers : finalReviewers
      }
-     
+
     await client.rest.pulls.requestReviewers(parameters);
     }
     catch(e){
